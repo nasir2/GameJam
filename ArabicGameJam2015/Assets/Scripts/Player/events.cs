b6afa9d05c8abf36c41ebbd064a76bbcfa42de05
Nasir Hussein <hussein.nasir@gmail.com>
2015-05-23 13:43:39 +0200

    Physics 2.0 by billy

---
+++ ArabicGameJam2015/Assets/Scripts/Player/events.cs
@@ -0,0 +1,19 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//	function OnCollisionStay2D(coll: Collision2D) {
+	//		if (coll.gameObject.tag == "Cube1"){
+	//			MeshRenderer
+
+	//		}
+//		}
+//	}*/
---
+++ ArabicGameJam2015/Assets/newScripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	
+	public float moveSpeed;
+	public float jumpHeight;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//playerController controller = GetComponent<playerController> ();
+		//if (controller.isGrounded)
+		//	print ("We are grounded, yeah man");
+
+		if (Input.GetKey (KeyCode.Space)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+			
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+			
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+			
+		}
+	}
+}
